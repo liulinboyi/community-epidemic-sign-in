b32e817d9c7f58fdeaf38ac298cf076c262c3520
liulinboyi <814921718@qq.com>
2021-05-28 17:51:50 +0800

    frat: init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Entry />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Entry from "./components/Entry.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Entry,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Entry.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Button class="btn">hello vant</Button>
+  </div>
+</template>
+
+<script>
+import { Button } from "vant";
+export default {
+  name: "Entry",
+  components: {
+    Button,
+  },
+  props: {
+    msg: String,
+  },
+  setup() {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.btn {
+  width: 100px;
+}
+</style>
